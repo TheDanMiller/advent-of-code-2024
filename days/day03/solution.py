cf2c090ae515c50241100f151c169d9a5a3354f4
Dan Miller <dan.miller@coforma.io>
2024-12-05 14:48:21 -0500

    WIP: finsihed day 03, framed day 04

--- days/day03/solution.py
+++ days/day03/solution.py
@@ -2,6 +2,31 @@ from common.utils import read_file, find_relative_path
 import re
 
 
+def remove_invalid_instructions(corrupted_line: str) -> str:
+    do_instructions = []
+    searching_index = 0
+    dont_search_string = "don't()"
+    do_search_string = "do()"
+
+    while True:
+        dont_index = corrupted_line.find(dont_search_string, searching_index)
+
+        #find dont
+        if dont_index == -1:
+            do_instructions.append(corrupted_line[searching_index:])
+            break
+        do_instructions.append(corrupted_line[searching_index: dont_index])
+        searching_index = dont_index + len(dont_search_string)
+
+        # find do, should not find don't because of inclusion of () in search string
+        do_index = corrupted_line.find(do_search_string, searching_index)
+        if do_index == -1:
+            break
+        searching_index = do_index + len(do_search_string)
+
+    return " ".join(do_instructions)
+
+
 def solve(use_sample: bool):
 
     # Resolve path relative to this script's directory
@@ -12,10 +37,12 @@ def solve(use_sample: bool):
 
     corrupted_memory = list(read_file(puzzle_input))  # Read file lines into a list
     corrupted_line = " ".join(corrupted_memory)
+    # pull out the don't sections
+    shorter_corrupted_string = remove_invalid_instructions(corrupted_line)
     regex = r"mul\((\d+),(\d+)\)"
 
     # Process each line
-    all_matches = re.findall(regex, corrupted_line)
+    all_matches = re.findall(regex, shorter_corrupted_string)
 
     # Print all matches found
     sum = 0
@@ -24,6 +51,3 @@ def solve(use_sample: bool):
         sum += delta
 
     print (f"The sum of the multiplied instructions is: {sum}")
-    # Regex to find matching patterns mul(int, int)
-    # multiple those ints and add to count
-    # print the heck out of the count
